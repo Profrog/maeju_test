35fda0bad8d6ea3a7cb73e9e3c9e8b457436ac83
mingyu <ache159@naver.com>
2021-08-12 22:58:25 +0900

    change

---
+++ app/src/main/java/com/example/logintest/ProfileActivity.java
@@ -0,0 +1,154 @@
+package com.example.logintest;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "ProfileActivity";
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+
+    private Button buttonLogout;
+    private TextView textivewDelete;
+
+    //추가
+    private TextView textViewUserLevel;
+    private TextView textViewHak;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //initializing views
+        textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        textivewDelete = (TextView) findViewById(R.id.textviewDelete);
+
+        //추가
+        textViewUserLevel=(TextView)findViewById(R.id.textviewUserLevel);
+        textViewHak=(TextView)findViewById(R.id.textviewHak);
+
+        String name="";
+        String email="";
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            for (UserInfo profile : user.getProviderData()) {
+                // Name, email address
+                name = profile.getDisplayName();
+                email = profile.getEmail();
+
+            }
+        }
+        String hakbun=email.substring(0,8);
+
+        String test="테스트";
+
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+        //유저가 로그인 하지 않은 상태라면 null 상태이고 이 액티비티를 종료하고 로그인 액티비티를 연다.
+        if(firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //유저가 있다면, null이 아니면 계속 진행
+
+        //textViewUserEmail의 내용을 변경해 준다.
+        textViewUserEmail.setText(user.getEmail()+"으로 로그인 하였습니다.");
+        textViewUserLevel.setText("안녕하세요 "+user.getDisplayName()+"님!");
+        textViewHak.setText("학번은 "+hakbun+"입니다.");
+
+        //logout button event
+        buttonLogout.setOnClickListener(this);
+        textivewDelete.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonLogout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        if(view == textivewDelete) {
+            AlertDialog.Builder myAlertBuilder =
+                    new AlertDialog.Builder(ProfileActivity.this);
+            // alert의 title과 Messege 세팅
+            myAlertBuilder.setTitle("경고");
+            myAlertBuilder.setMessage("정말 회원 탈퇴를 진행하시겠습니까?");
+            // 버튼 추가 (Ok 버튼과 Cancle 버튼 )
+            myAlertBuilder.setPositiveButton("예",new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog,int which){
+                    // OK 버튼을 눌렸을 경우
+
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    user.delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d(TAG, "회원 탈퇴가 완료되었습니다.");
+                                    }
+                                }
+                            });
+                    Toast.makeText(getApplicationContext(),"회원 탈퇴가 완료되었습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            myAlertBuilder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Cancle 버튼을 눌렸을 경우
+                    Toast.makeText(getApplicationContext(),"회원 탈퇴가 취소되었습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            // Alert를 생성해주고 보여주는 메소드(show를 선언해야 Alert가 생성됨)
+            myAlertBuilder.show();
+
+        }
+    }
+
+    public boolean onSupportNavigateUp(){
+        onBackPressed();; // 뒤로가기 버튼이 눌렸을시
+        return super.onSupportNavigateUp(); // 뒤로가기 버튼
+    }
+
+
+    public void enter(View v)
+    {
+        Intent intent1 = new Intent(getApplicationContext(), Showin.class);
+        startActivity(intent1);
+    }
+
+}
---
+++ app/src/main/java/com/example/logintest/Showin.java
@@ -0,0 +1,88 @@
+package com.example.logintest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import android.widget.Toast;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import java.util.Collections;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Showin extends AppCompatActivity{
+
+    private String name02;
+    private String id02;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_in);
+        name02 = "김민규";
+        id02 = "12161538";
+        getDataFromAPI();
+    }
+
+    private void getDataFromAPI() {
+        // creating a string variable for URL.
+        String url = "https://spreadsheets.google.com/feeds/list/1v8E3EcVawJsX0US5boll4QYPK_qHm3Tt71ZYY4uVwMo/od6/public/values?alt=json";
+
+        // creating a new variable for our request queue
+        RequestQueue queue = Volley.newRequestQueue(Showin.this);
+
+        // creating a variable for our JSON object request and passing our URL to it.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject feedObj = response.getJSONObject("feed");
+                    JSONArray entryArray = feedObj.getJSONArray("entry");
+                    for(int i=0; i<entryArray.length(); i++){
+                        JSONObject entryObj = entryArray.getJSONObject(i);
+                        String id_excel = entryObj.getJSONObject("gsx$id").getString("$t");
+                        String name_excel = entryObj.getJSONObject("gsx$name").getString("$t");
+                        String mp_excel = entryObj.getJSONObject("gsx$mainpoint").getString("$t");
+                        String qp_excel = entryObj.getJSONObject("gsx$quizpoint").getString("$t");
+                        //userModalArrayList.add(new UserModal(firstName, lastName, email, avatar));
+
+                        System.out.println("heelo" + id02);
+
+                        if(id_excel.equals(id02)){
+                            TextView m1 = (TextView)findViewById(R.id.mainpoint);
+                            TextView q1 = (TextView)findViewById(R.id.quizpoint);
+                            m1.setText(mp_excel);
+                            q1.setText(qp_excel);
+                            break;
+                        }
+                        //System.out.println("heelo" + firstName);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // handline on error listener method.
+                //Toast.makeText(MainActivity.this, "Fail to get data..", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // calling a request queue method
+        // and passing our json object
+        queue.add(jsonObjectRequest);
+    }
+}
