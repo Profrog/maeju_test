286f653700306d1132d3e1a5f0cf91a3c8e85b10
shinyumi43 <yumishin43@gmail.com>
2021-08-22 14:13:36 +0900

    change 1 ~ 20 week

--- app/src/main/java/com/example/logintest/Configuration.java
+++ app/src/main/java/com/example/logintest/Configuration.java
@@ -3,7 +3,7 @@ package com.example.logintest;
 public class Configuration {
     // google apps script 연결, 자체 제작 api 활용
     // 이미지 파일이 현재 공유 문서함 드라이브에 기재되지 않는 문제 존재
-    public static final String APP_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzyAWZ9IdbwE7Zd7TWnAIHqvLtq6OO-fAOTxpknB6Jye4S0jEHc6ZgkqsDWBHPz-9MCmw/exec";
+    public static final String APP_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxoCId7M8Dr4a9ux0xT7WxzJRXKezVoRfukf9O1kwjMzqABqvC88inoxv1qCjh7cCTdzQ/exec";
     public static final String ADD_USER_URL = APP_SCRIPT_WEB_APP_URL;
 
     // 학번, 이름, 이미지 변수
