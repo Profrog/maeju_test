21532fcb76f030e2104f997b34839425e27f681d
kite707 <lin102501@gmail.com>
2021-08-27 23:49:16 +0900

    상단바 없애기

--- app/src/main/java/com/example/logintest/StartActivity.java
+++ app/src/main/java/com/example/logintest/StartActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -42,6 +44,7 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_start);
 
         buttonSignup = (Button) findViewById(R.id.mvtosignup);
